5a8b44973118cc20d5a8342550b953fd9bde013c
princebyun <princebyun@gmail.com>
2025-10-23 21:45:14 +0900

    docs : 구현할 기능 목록 정리
    - README 파일 기능 목록 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -28,6 +28,17 @@ class CarTest {
         assertThat(car1.getName()).isEqualTo(name);
     }
 
+    @Test
+    @DisplayName("자동차 클래스 객체 생성 실패 테스트")
+    void car_create_test_false() {
+        String name = "princebyun";
+
+        Car car1 = new Car(name);
+
+        assertThat(car1).isNotNull();
+        assertThat(car1.getName()).isNotEqualTo("princebyunFalse");
+    }
+
     @AfterEach
     void tearDown() {
         System.out.println("자동차 클래스 테스트 완료");
