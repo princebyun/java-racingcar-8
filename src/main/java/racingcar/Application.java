59b6172b031c03604cace03482e7697393d0c2a1
princebyun <princebyun@gmail.com>
2025-10-26 22:50:53 +0900

    refactor : 2주차 피드백 적용
    - 의미없는 주석을 달지 않는 피드백 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,22 +6,17 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        //입력 및 출력
         UserInput userInput = new UserInput();
         RacingOutput racingOutput = new RacingOutput();
 
-        //자동차 컬렉션 생성
         String carsName = userInput.carsNameInput();
         Cars cars = new Cars(carsName);
 
-        //자동차 경주 생성
         String racingCount = userInput.racingRoundInput();
         Racing racing = new Racing(racingCount);
 
-        //경주 진행
         List<Car> carList = racing.running(cars.getCars());
 
-        //경주 우승자
         racingOutput.racingResultOutput(racing.resultWinners(carList));
 
     }
